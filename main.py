8ba6fa29c0d27947e735b0d530e1292a7a620b37
0811j <0811jisoo@gmail.com>
2021-11-16 15:35:52 +0900

    네이버 영화 제목, 리뷰, 점수, 작성자, 날짜 수집(+모듈화)

--- main.py
+++ main.py
@@ -10,10 +10,10 @@
 # 만든이: soo811
 # 일자: 2021. 11. 09
 
+import math
 import requests
 from bs4 import BeautifulSoup
-import webcrawl.WevCrawlService as wcs
-
+import webcrawl.WebCrawlService as wcs
 
 ######################
 # 1.데이터 수집 및 저장 #
@@ -22,10 +22,19 @@ import webcrawl.WevCrawlService as wcs
 movie_code = '209496'  # 네이버 영화 code
 
 # 1.제목 수집
-
-
 title = wcs.get_movie_title(movie_code)
 print(title)
+
+# 2.전체 페이지수 계산
+pages = wcs.calc_pages(movie_code)
+print(pages)
+
+# 3.리뷰 수집
+wcs.get_reviews(movie_code, pages, title)
+
+
+
+
 
 
 ################
